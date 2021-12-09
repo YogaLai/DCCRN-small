e21b551c743412e23d2c6b3c77688d8f99a4031d
YogaLai <yoga.lai1025@gmail.com>
2021-12-09 16:22:13 +0800

    visualize spectrogram

--- utils.py
+++ utils.py
@@ -8,6 +8,10 @@ from __future__ import division
 from __future__ import print_function
 import torch
 import matplotlib.pyplot as plt
+import soundfile as sf
+import librosa
+import librosa.display
+import numpy as np
 
 def show_params(nnet):
     print("=" * 40, "Model Parameters", "=" * 40)
@@ -31,7 +35,6 @@ def show_model(nnet):
             print(m)
     print("=" * 98)
 
-
 def visualize_mask(mask):
     mask_mag = mask[0][0]
     plt.imshow(mask_mag)
@@ -44,3 +47,33 @@ def spec2complex(x, fft_len):
     imag = x[:,fft_len//2+1:]
 
     return torch.complex(real, imag)
+
+# def visualize_spec(noisy_path, clean_path, estimate_path):
+#     noisy, sample_rate = sf.read(noisy_path)
+#     clean, sample_rate = sf.read(clean_path)
+#     estimate, sample_rate = sf.read(estimate_path)
+#     # spec = stft(wav)
+#     plt.subplot(1,3,1)
+#     plt.specgram(noisy, 512, sample_rate, cmap='jet')
+#     plt.subplot(1,3,2)
+#     plt.specgram(clean, 512, sample_rate, cmap='jet')
+#     plt.subplot(1,3,3)
+#     plt.specgram(estimate, 512, sample_rate, cmap='jet')
+#     plt.show()
+
+def save_spec_img(path, filename):
+    wav, sample_rate = librosa.load(path)
+    spec = librosa.stft(wav)
+    spec = librosa.amplitude_to_db(np.abs(spec), ref=np.max)
+    plt.figure()
+    librosa.display.specshow(spec)
+    plt.colorbar()
+    plt.savefig(f'visualization/{filename}.png')
+  
+def visualize_spec():
+    save_spec_img('D:/yoga/noisy.wav', 'noisy')
+    save_spec_img('D:/yoga/clean.wav', 'clean')
+    save_spec_img('D:/yoga/estimate.wav', 'estimate')
+
+if __name__ == '__main__':
+    visualize_spec()
